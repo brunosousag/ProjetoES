a9f75f387bb2b77a3a5355955fe8990783ac49b8
João Robalo <joao.diogo.robalo@gmail.com>
2026-05-16 18:13:18 +0100

    ligação da pagina historico vendas para imprimir fatura

--- ProjetoEsoft/src/main/java/org/modelo/HistoricoVendas.java
+++ ProjetoEsoft/src/main/java/org/modelo/HistoricoVendas.java
@@ -35,15 +35,41 @@ public class HistoricoVendas extends BaseFrame {
         super.btnCarrinho = btnCarrinho;
 
         configurarMenuGestao();
+        preencherListas();
+        paginaImprimir();
 
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    public void paginaImprimir() {
+        imprimirButton.addActionListener(e -> {
+
+            ImprimirFatura janela =
+                    new ImprimirFatura("Campeonato Mundial 2026 - Imprimir Fatura");
+
+            janela.setVisible(true);
+        });
+    }
+
+    public void preencherListas(){
+        //Centrar texto das listas
         DefaultListCellRenderer renderer = new DefaultListCellRenderer();
         renderer.setHorizontalAlignment(SwingConstants.CENTER);
 
         list1.setCellRenderer(renderer);
         list2.setCellRenderer(renderer);
         list3.setCellRenderer(renderer);
-
-        pack();
-        setLocationRelativeTo(null);
     }
+
+    //Descomentar para testar página
+    /*
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new HistoricoVendas("Campeonato Mundial 2026 - Histórico de Vendas").setVisible(true);
+        });
+    }
+     */
+
+
 }
