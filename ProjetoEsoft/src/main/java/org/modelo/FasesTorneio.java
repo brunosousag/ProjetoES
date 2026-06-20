c2df47341f2e232624ee31293bbb28675250ca31
João Robalo <joao.diogo.robalo@gmail.com>
2026-06-20 17:15:53 +0100

    labels bracket renomeadas

--- ProjetoEsoft/src/main/java/org/modelo/FasesTorneio.java
+++ ProjetoEsoft/src/main/java/org/modelo/FasesTorneio.java
@@ -8,6 +8,39 @@ public class FasesTorneio extends BaseFrame {
     private JButton btnMerch;
     private JButton btnGestao;
     private JPanel janelaBracket;
+    private JLabel lblOitavos_jogo1_equipa1;
+    private JLabel lblOitavos_jogo1_equipa2;
+    private JLabel lblOitavos_jogo2_equipa1;
+    private JLabel lblOitavos_jogo2_equipa2;
+    private JLabel lblOitavos_jogo3_equipa1;
+    private JLabel lblOitavos_jogo3_equipa2;
+    private JLabel lblOitavos_jogo4_equipa1;
+    private JLabel lblOitavos_jogo4_equipa2;
+    private JLabel lblOitavos_jogo5_equipa1;
+    private JLabel lblOitavos_jogo5_equipa2;
+    private JLabel lblOitavos_jogo6_equipa1;
+    private JLabel lblOitavos_jogo6_equipa2;
+    private JLabel lblOitavos_jogo7_equipa1;
+    private JLabel lblOitavos_jogo7_equipa2;
+    private JLabel lblOitavos_jogo8_equipa1;
+    private JLabel lblOitavos_jogo8_equipa2;
+    private JLabel lblQuartos_jogo1_equipa1;
+    private JLabel lblQuartos_jogo1_equipa2;
+    private JLabel lblQuartos_jogo2_equipa1;
+    private JLabel lblQuartos_jogo2_equipa2;
+    private JLabel lblQuartos_jogo3_equipa1;
+    private JLabel lblQuartos_jogo3_equipa2;
+    private JLabel lblQuartos_jogo4_equipa1;
+    private JLabel lblQuartos_jogo4_equipa2;
+    private JLabel lblMeia_jogo1_equipa1;
+    private JLabel lblMeia_jogo1_equipa2;
+    private JLabel lblMeia_jogo2_equipa1;
+    private JLabel lblMeia_jogo2_equipa2;
+    private JLabel lblFinal_jogo1_equipa1;
+    private JLabel lblFinal_jogo1_equipa2;
+    private JLabel lblVencedor;
+
+
 
     public FasesTorneio(String title) {
         super(title);
